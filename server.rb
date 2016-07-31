96375138a122d7de315f60c69d33bd9f7e2081bc
Ondřej Želazko <zelazko@insiaoffice.cz>
2016-07-31 17:02:46 +0200

    cv texts

--- server.rb
+++ server.rb
@@ -4,7 +4,16 @@ get '/' do
   erb :home
 end
 
+get '/cv' do
+  erb :cv
+end
+
 get '/img/:num.png' do
   cache_control :public, max_age: 3600
   send_file "assets/#{params[:num]}.png"
+end
+
+get '/photo.jpg' do
+  cache_control :public, max_age: 3600
+  send_file 'assets/photo.jpg'
 end
