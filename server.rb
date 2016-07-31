8ae7eb79ffdf849f2d5c71ed59d23159c6bc98cb
Ondřej Želazko <zelazko@insiaoffice.cz>
2016-07-31 18:17:27 +0200

    cv done

--- server.rb
+++ server.rb
@@ -8,6 +8,11 @@ get '/cv' do
   erb :cv
 end
 
+get '/cv/klara_halamkova.pdf' do
+  cache_control :public, max_age: 3600
+  send_file 'assets/klara_halamkova.pdf'
+end
+
 get '/img/:num.png' do
   cache_control :public, max_age: 3600
   send_file "assets/#{params[:num]}.png"
